d0272d1b6a6eac05d8748b510e8a0efb4d0588d5
Rishikesh Mane <139518215+RishiMane03@users.noreply.github.com>
2023-09-29 17:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your code here
+const draggables = document.querySelectorAll('.image');
+// console.log(draggables);
+
+    draggables.forEach((draggable) => {
+
+            draggable.addEventListener("dragstart", (event) => {
+                event.dataTransfer.setData("id", draggable.id);
+            });
+
+            draggable.addEventListener("dragover", (event) => {
+                event.preventDefault();
+            });
+
+            draggable.addEventListener('drop', (event) => {
+                const droppedElementId = event.dataTransfer.getData("id");
+                const element1 = document.getElementById(droppedElementId);
+                const element2 = event.target;
+
+                // Swap the IDs
+                const temp = element1.id;
+                element1.id = element2.id;
+                element2.id = temp;
+            });
+    });
